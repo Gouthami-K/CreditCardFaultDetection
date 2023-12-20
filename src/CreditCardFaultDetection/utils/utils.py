a8c11a377fb7beb29f17a54848723671ec1cd03f
Gouthami-K <gouthamikrishnamurthy@gmail.com>
2023-12-20 14:26:24 +0530

    changes made in utils and indext.html

--- src/CreditCardFaultDetection/utils/utils.py
+++ src/CreditCardFaultDetection/utils/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 from src.CreditCardFaultDetection.logger import logging
 from src.CreditCardFaultDetection.exception import customexception
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,roc_auc_score,confusion_matrix,classification_report
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import GridSearchCV
 
@@ -38,9 +38,21 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
             train_model_score = accuracy_score(y_train, y_train_pred)
             test_model_score = accuracy_score(y_test, y_test_pred)
 
+            # Precision
+            Precision = precision_score(y_test, y_test_pred)
+
+            # Recall score for test data
+            
+            recall_score_value = recall_score(y_test, y_test_pred)
+
+            # f1 score for test data
+            f1 = f1_score(y_test, y_test_pred)
+
+            # Auc Roc
+            AUCROC=roc_auc_score(y_test, y_test_pred)
 
             # Store scores in the report dictionary
-            report[model_name] = {'train_accuracy': train_model_score, 'test_accuracy': test_model_score}
+            report[model_name] = {'train_accuracy': train_model_score, 'test_accuracy': test_model_score,'precision_score':Precision, 'recall_score':recall_score_value,'f1_score':f1,'roc_auc_score':AUCROC}
 
         return report
 
@@ -141,7 +153,7 @@ def replace_categories(df):
         raise customexception(e, sys)
 
 
-      
+
 def rename_and_drop(df):
     try:
         df['IsDefaulter'] = df['default.payment.next.month']

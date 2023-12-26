ee446c5259cb3ce510497ef0ddf2a36c37038dd6
Gouthami-K <gouthamikrishnamurthy@gmail.com>
2023-12-27 01:21:57 +0530

    added mlflow and dvc

--- src/CreditCardFaultDetection/pipelines/training_pipeline.py
+++ src/CreditCardFaultDetection/pipelines/training_pipeline.py
@@ -1,6 +1,7 @@
 from src.CreditCardFaultDetection.components.data_ingestion import DataIngestion
 from src.CreditCardFaultDetection.components.data_transformation import DataTransformation
 from src.CreditCardFaultDetection.components.model_trainer import ModelTrainer
+from src.CreditCardFaultDetection.components.model_evaluation import ModelEvaluation
 
 import os
 import sys
@@ -16,3 +17,6 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
+
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
